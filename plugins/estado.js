98aa846eb8d59d6e7ccb77b70e9394d0e9b5fcf4
agromos1 <101301534+agromos1@users.noreply.github.com>
2022-03-09 19:52:51 -0600

    Update estado.js

--- plugins/estado.js
+++ plugins/estado.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.send3Button(m.chat, `
-*ミ💖 Hola estimado usuario 💖彡*
+*ミ💚 Hola estimado usuario 💚彡*
 
 *ミ🤖 Estado del Bot 🤖彡*
 *=> Bot activo ✔️*
 *=> Bot uso público ✔️*
-`.trim(), '©The Shadow Borkers - Bot', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '🔶️ MENÚ SIMPLE 🔶️', `${usedPrefix}menusimple`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menuaudios`, m, false, { contextInfo: { mentionedJid }})}
+`.trim(), 'AGROMOS SP - Bot', '☣ MENÚ ☣', `${usedPrefix}menu`, '⚜ MENÚ SIMPLE ⚜', `${usedPrefix}menusimple`, '✳ MENÚ AUDIOS ✳', `${usedPrefix}menuaudios`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(estado|status|estate|state|stado|stats)$/i
 module.exports = handler
